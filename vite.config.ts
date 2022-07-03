149ac28e75fb43b985cef9d536c32dc47f4e1406
Sumuyzzz <Sumuyzzz@gmail.com>
2022-07-04 01:43:16 +0800

    🐳 chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,29 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({
-    script: {
-      refSugar: true,
-      refTransform: true,
-    },
-  })],
+  plugins: [
+    AutoImport({
+      dts: true,
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }), vue({
+      script: {
+        refSugar: true,
+        refTransform: true,
+      },
+
+    }),
+  ],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
   base: '/note_vue3/',
 })
+
